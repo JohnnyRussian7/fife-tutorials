8e7212ae84e42369bcc0d0b433cd1405f8d004cd
manning.jesse@gmail.com <manning.jesse@gmail.com@38a4def6-a25c-11de-9e49-a714085a53fc>
2011-08-28 15:37:11 +0000

    XCode project should build again, had to put back in namespace for Rect to compile.

--- branches/Engine_0_2/src/geometry/Rect.h
+++ branches/Engine_0_2/src/geometry/Rect.h
@@ -23,47 +23,49 @@
 #define RECT_H_
 
 #include "../StdIncludes.h"
-#include "math/MathUtil.h"
+#include "../math/MathUtil.h"
 
-template <typename T>
-struct Rect
-{
-    Rect() : m_left(T(0)), m_top(T(0)), m_right(T(0)), m_bottom(T(0)) { }
-    Rect(T left, T top, T right, T bottom)
-    : m_left(left), m_top(top), m_right(right), m_bottom(bottom)
+namespace fife {
+    template <typename T>
+    struct Rect
     {
+        Rect() : m_left(T(0)), m_top(T(0)), m_right(T(0)), m_bottom(T(0)) { }
+        Rect(T left, T top, T right, T bottom)
+        : m_left(left), m_top(top), m_right(right), m_bottom(bottom)
+        {
+            
+        }
         
+        inline bool operator==(const Rect& rhs);
+        inline bool operator!=(const Rect& rhs);
+        
+        T m_left;
+        T m_top;
+        T m_right;
+        T m_bottom;
+    };
+    
+    template <typename T>
+    bool Rect<T>::operator==(const Rect& rhs)
+    {
+        return (IsEqual(m_left, rhs.m_left)
+                && IsEqual(m_top, rhs.m_top)
+                && IsEqual(m_right, rhs.m_right)
+                && IsEqual(m_bottom, rhs.m_bottom));
     }
     
-    inline bool operator==(const Rect& rhs);
-    inline bool operator!=(const Rect& rhs);
-
-    T m_left;
-    T m_top;
-    T m_right;
-    T m_bottom;
-};
-
-template <typename T>
-bool Rect<T>::operator==(const Rect& rhs)
-{
-    return (IsEqual(m_left, rhs.m_left)
-            && IsEqual(m_top, rhs.m_top)
-            && IsEqual(m_right, rhs.m_right)
-            && IsEqual(m_bottom, rhs.m_bottom));
+    template <typename T>
+    bool Rect<T>::operator!=(const Rect& rhs)
+    {
+        return (!IsEqual(m_left, rhs.m_left)
+                || !IsEqual(m_top, rhs.m_top)
+                || !IsEqual(m_right, rhs.m_right)
+                || !IsEqual(m_bottom, rhs.m_bottom));
+    }
 }
 
-template <typename T>
-bool Rect<T>::operator!=(const Rect& rhs)
-{
-    return (!IsEqual(m_left, rhs.m_left)
-            || !IsEqual(m_top, rhs.m_top)
-            || !IsEqual(m_right, rhs.m_right)
-            || !IsEqual(m_bottom, rhs.m_bottom));
-}
-
-typedef Rect<float> FloatRect;
-typedef Rect<uint32_t> UintRect;
-typedef Rect<int32_t> IntRect;
+typedef fife::Rect<float> FloatRect;
+typedef fife::Rect<uint32_t> UintRect;
+typedef fife::Rect<int32_t> IntRect;
 
 #endif
